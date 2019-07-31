902c1cc5b6a1622889e77b637e30c492797db607
吴培涛 <peitao.wu@ryit.co>
2019-07-31 11:09:05 +0800

    创建androidx分支

--- app/src/main/java/co/ryit/App.java
+++ app/src/main/java/co/ryit/App.java
@@ -1,18 +1,7 @@
 package co.ryit;
 
 import android.app.Application;
-import android.text.TextUtils;
 
-
-import com.umeng.commonsdk.UMConfigure;
-import com.umeng.socialize.PlatformConfig;
-
-
-import cn.jpush.im.android.api.JMessageClient;
-import co.baselib.global.AppController;
-import co.baselib.global.IloomoConfig;
-import co.jmessage.JmessageConfig;
-import co.wpt.utils.UmengUtils;
 
 public class App extends Application {
 
@@ -20,15 +9,6 @@ public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        UmengUtils.setShareUtilsInit(this, "59150911b27b0a1ebf002350", true);
-        IloomoConfig.init(this).setDebug(false);
-        AppController.getInstance().init(this, this);
-        JmessageConfig.init(this, true);
-    }
 
-    {
-        PlatformConfig.setWeixin(BuildConfig.wxAPPID, BuildConfig.wxSecret);
-        PlatformConfig.setQQZone(BuildConfig.qqAPPID, BuildConfig.qqKey);
-        PlatformConfig.setSinaWeibo(BuildConfig.wbKEY, BuildConfig.wbSecret, BuildConfig.wbRedirectUrl);
     }
 }
--- app/src/main/java/co/ryit/global/HttpTaskID.java
+++
@@ -1,8 +0,0 @@
-package co.ryit.global;
-
-/**
- * Created by wupeitao on 2017/4/14.
- */
-
-public class HttpTaskID {
-}
